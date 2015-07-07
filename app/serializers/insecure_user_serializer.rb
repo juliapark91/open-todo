98b9fc022cd7a6c08da8ef2e13dd6534f1695ddd
Julia Park <juliapark91@gmail.com>
2015-07-07 10:16:55 -0700

    updated attributes of user_serializer

--- app/serializers/insecure_user_serializer.rb
+++
@@ -1,7 +0,0 @@
-class InsecureUserSerializer < ActiveModel::Serializer
-  attributes :id, :email, :password
-
-  def email
-    object.(self.email)
-  end
-end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,11 +1,3 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :email, :password
-
-  # def email
-  #   object.email
-  # end
-
-  def created_at
-    object.created_at.strftime('%B %d, %Y')
-  end
 end
