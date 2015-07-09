d2f3e735a0f25423c79147d8b1a13721d29252e2
Conrad Taylor <conradwt@gmail.com>
2015-07-09 16:02:57 -0700

    Remove unused test file.

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,8 +1,29 @@
-class Api::UsersController < ApiController
+module API
 
-  def index
-    @users = User.all
-    render json: @users, each_serializer: UserSerializer 
+  class UsersController < APIController
+
+    def index
+      @users = User.all
+      
+      render json: @users, each_serializer: UserSerializer 
+    end
+  
+    def create 
+      @user = User.new( user_params )
+      
+      if @user.save
+        render json: { success: "user sucessfully created" }, status: :created
+      else
+        render json: { failure: "invalid e-mail or password" }, status: :unprocessable_entity
+      end
+    end
+
+  end
+  
+private 
+  
+  def user_params
+    params.require( :user ).permit( :email, :password )
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   namespace :api, defaults: { format: :json } do
-    resources :users, only: [:index]
+    resources :users, only: [:index, :create]
   end
   
 end
