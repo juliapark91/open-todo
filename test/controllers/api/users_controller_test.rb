f7172fa472c22030eb9023ac2920e5aed3953e10
Conrad Taylor <conradwt@gmail.com>
2015-07-09 15:34:36 -0700

    Add user creation tests.

--- test/controllers/api/users_controller_test.rb
+++ test/controllers/api/users_controller_test.rb
@@ -1,9 +1,90 @@
 require 'test_helper'
 
-class Api::UsersControllerTest < ActionController::TestCase
-  
-  test 'should get index' do
-    get :index
+module API
+
+  class UsersControllerTest < ActionDispatch::IntegrationTest
+    
+    #
+    # curl -X POST -H "Accept: application/json" -H "Content-Type: application/json" -d '{"user":{"email":"someone@example.com","password":"password"}}' http://localhost:3000/api/users
+    #
+
+    test "POST /users should create user with valid email and valid password" do
+
+      assert_difference( 'User.count' ) do
+        post '/api/users', 
+          { user: 
+            { email: 'someone@example.com', password: 'password' }
+          }.to_json,
+          { 'Accept': Mime::JSON, 'Content-Type': Mime::JSON.to_s }
+      end
+
+      assert_equal 201, response.status
+      assert_equal Mime::JSON, response.content_type
+      
+      response_message = json( response.body )
+      assert_equal( { success: "user sucessfully created" }, response_message )
+    end
+    
+    #
+    # curl -X POST -H "Accept: application/json" -H "Content-Type: application/json" -d '{"user":{"email":"someone@example.com", "password":""}}' http://localhost:3000/api/users
+    #
+
+    test "POST /users should not create user with valid email and invalid password" do
+      assert_no_difference( 'User.count' ) do
+        post '/api/users', 
+          { user: 
+            { email: 'someone@example.com', password: nil }
+          }.to_json,
+          { 'Accept': Mime::JSON, 'Content-Type': Mime::JSON.to_s }
+      end
+
+      assert_equal 422, response.status
+      assert_equal Mime::JSON, response.content_type
+      
+      response_message = json( response.body )
+      assert_equal( { failure: "invalid e-mail or password" }, response_message )
+    end
+    
+    #
+    # curl -X POST -H "Accept: application/json" -H "Content-Type: application/json" -d '{"user":{"email": "", "password":"password"}}' http://localhost:3000/api/users
+    #
+
+    test "POST /users should not create user with an invalid email and valid password" do
+      assert_no_difference( 'User.count' ) do
+        post '/api/users', 
+          { user: 
+            { email: nil, password: 'password' }
+          }.to_json,
+          { 'Accept': Mime::JSON, 'Content-Type': Mime::JSON.to_s }
+      end
+
+      assert_equal 422, response.status
+      assert_equal Mime::JSON, response.content_type
+      
+      response_message = json( response.body )
+      assert_equal( { failure: "invalid e-mail or password" }, response_message )
+    end
+    
+    #
+    # curl -X POST -H "Accept: application/json" -H "Content-Type: application/json" -d '{"user":{"email":"","password":""}}' http://localhost:3000/api/users
+    #
+
+    test "POST /users should not creates user with invalid email and valid password" do
+      assert_no_difference( 'User.count' ) do
+        post '/api/users', 
+          { user: 
+            { email: nil, password: nil }
+          }.to_json,
+          { 'Accept': Mime::JSON, 'Content-Type': Mime::JSON.to_s }
+      end
+
+      assert_equal 422, response.status
+      assert_equal Mime::JSON, response.content_type
+      
+      response_message = json( response.body )
+      assert_equal( { failure: "invalid e-mail or password" }, response_message )
+    end
+    
   end
-  
+
 end
