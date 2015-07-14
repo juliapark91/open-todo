4fab755155da6271f451a7dcc0658a94d7a7f946
Julia Park <juliapark91@gmail.com>
2015-07-14 16:48:54 -0700

    complete with curl testing

--- app/controllers/api/items_controller.rb
+++ app/controllers/api/items_controller.rb
@@ -5,12 +5,23 @@ module API
     before_action :authenticated?
   
     def create 
-      @item = Item.new( item_params )
+      @list = List.find(params[:list_id])
+      @item = @list.items.new( item_params )
       
       if @item.save
         render json: { success: "item sucessfully created" }, status: :created
       else
         render json: { failure: "invalid item name" }, status: :unprocessable_entity
+      end
+    end
+
+    def update
+      @list = List.find(params[:list_id])
+      @item = @list.items.find(params[:id])
+      if @item.update( item_params )
+        render json: { success: "Item update completed" }, status: :updated
+      else
+        render json: { errors: @item.errors.full_messages }, status: :unprocessable_entity
       end
     end
     
--- app/controllers/api/lists_controller.rb
+++ app/controllers/api/lists_controller.rb
@@ -1,11 +1,12 @@
 module API
 
   class ListsController < APIController
-    
+
     before_action :authenticated?
+    before_action :set_user
   
     def create 
-      @list = List.new( list_params )
+      @list = @user.lists.new( list_params )
       
       if @list.save
         render json: { success: "list sucessfully created" }, status: :created
@@ -16,18 +17,31 @@ module API
 
     def destroy
       begin
-        list = List.find(params[:id])
-        list.destroy
+        @list = @user.lists.find(params[:id])
+        @list.destroy
         render json: {}, status: :no_content
       rescue ActiveRecord::RecordNotFound
         render :json => {}, :status => :not_found
+      end
+    end
+
+    def update
+      @list = @user.lists.find(params[:id])
+      if @list.update( list_params )
+        render json: @list
+      else
+        render json: { errors: @list.errors.full_messages }, status: :unprocessable_entity
       end
     end
     
   private 
   
     def list_params
-      params.require( :list ).permit( :name )
+      params.require( :list ).permit( :name, :permissions )
+    end
+
+    def set_user
+      @user = User.find(params[:user_id])
     end
 
   end
